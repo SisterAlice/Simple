ed899dc2b16a1360372c05bff621ea815a758ced
PorisulkiP <alex1228@gmail.com>
2020-03-02 11:14:58 +0300

    basic functionality

--- 1.py
+++ 1.py
@@ -1,4 +1,13 @@
 from tkinter import *
 
+
+def add_label():
+    Label(root, text="New label", padx='30', pady='20', fg='white',bg="gray").pack(side = BOTTOM, fill=X)
+
+
 root = Tk()
+root.geometry("300x500")
+
+Label(root, text="Just test", padx='30', pady='0').pack(side = BOTTOM)
+Button(root, text="Click me!", command=add_label).pack()
 root.mainloop()
